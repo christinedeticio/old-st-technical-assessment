bbb2e72948790d18f553b87cf5c7d70a2567ce1a
christinedeticio <christine_deticio@dlsu.edu.ph>
2022-05-25 01:03:22 +0800

    Updated header design

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ function App() {
   return (
     <div className='App'>
       <header>
-          <h1>Products Grid</h1>
+          <h1>Old St. eCommerce Products</h1>
 
-          <p>But first, a word from our sponsors:</p>
-          <img
-            className='ad'
-            src={`http://localhost:8000/ads/?r=${Math.floor(Math.random()*1000)}`}
-            alt='ad'/>
+          <div className='headerAd'>
+            <p>But first, a word from our sponsors:</p>
+            <img
+              className='ad'
+              src={`http://localhost:8000/ads/?r=${Math.floor(Math.random()*1000)}`}
+              alt='ad'/>
+          </div>
       </header>
-
-      
 
       <Products />
     </div>
