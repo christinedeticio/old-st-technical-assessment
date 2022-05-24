91115fd8d6f791854e8e796710d37211ab33ab13
christinedeticio <christine_deticio@dlsu.edu.ph>
2022-05-25 01:53:27 +0800

    Updated header

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className='App'>
       <header>
-          <h1>Old St. eCommerce Products</h1>
+          <h1>Old St eCommerce Products</h1>
 
           <div className='headerAd'>
             <p>But first, a word from our sponsors:</p>
