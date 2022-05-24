0209a9dbee67a569658324b96bfcbfecaa10fa65
christinedeticio <christine_deticio@dlsu.edu.ph>
2022-05-24 15:23:01 +0800

    Fixed product date and price format

--- src/Product.js
+++ src/Product.js
@@ -2,6 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Product extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    formatDate() {
+        
+        let currDate = new Date()
+        let productDate = new Date(this.props.date);
+
+        let dayDiff = Math.floor((currDate.getTime() - productDate.getTime()) / (1000 * 60 * 60 * 24));
+
+        if(dayDiff < 7) {
+            return dayDiff + (dayDiff === 1 ? " day ago" : " days ago");
+        }
+        else {
+            let dateString = productDate.toString().split(" ");
+            return(dateString[1] + " " + dateString[2] + ", " + dateString[3])
+        }
+    }
+    
     render() {
         return (
             <div>
@@ -9,11 +29,11 @@ class Product extends React.Component {
                     {this.props.face}
                 </div>
                 <div>
-                    {this.props.size}
-                    {this.props.date}
+                    {this.formatDate()}
                 </div>
                 <div>
-                    {this.props.price}
+                    {this.props.size} pixels
+                    ${this.props.price / 100}
                 </div>
             </div>
         );
